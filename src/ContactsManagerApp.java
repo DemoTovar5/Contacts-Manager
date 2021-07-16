6f3b86cf0f712324a7723c7ed773f8286c14a710
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-16 14:29:20 -0500

    Lists

--- src/ContactsManagerApp.java
+++ src/ContactsManagerApp.java
@@ -1,5 +1,50 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class ContactsManagerApp {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args)
+    {
+        final Scanner scanner = new Scanner(System.in);
+        while (true) {
+
+            System.out.println("   ~ Contact List ~");
+            System.out.println("+-------------------------+");
+            System.out.println(" 1. Lists contacts");
+            System.out.println(" 2. Add contacts");
+            System.out.println(" 3. Search contacts");
+            System.out.println(" 4. Delete contacts");
+            System.out.println(" 5. Exit");
+            System.out.println("+-------------------------+");
+            scanner.useDelimiter("\n");
+           Integer choice = scanner.nextInt();
+
+
+            if (choice <= 0 || choice > 5) {
+                System.out.println("Invalid selection. ");
+
+                List<String> currentList = new ArrayList<>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+
     }
+
+
+
+
+
 }
+
+
