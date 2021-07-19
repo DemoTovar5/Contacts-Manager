898e2b1afa2a793ca5210ee7647c2eae495930dd
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-19 10:45:12 -0500

    changes

---
+++ src/Contacts.java
@@ -0,0 +1,35 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Contacts {
+
+
+
+    public static void listContacts() {
+        Path toDataFolder = Paths.get("src/data");
+        Path toDataFile = Paths.get(String.valueOf(toDataFolder), "contacts.txt");
+        List<String> currentList = new ArrayList<>();
+
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
